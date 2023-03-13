468e88be640f56c6122ee155db8feeef8ae1e890
Mariana Faustino <marifaustino@gmail.com>
2023-03-13 15:34:10 -0300

    configurando react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
