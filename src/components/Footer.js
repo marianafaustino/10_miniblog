50ed313b8e00206240b29b861959786c78fb7b6c
Mariana Faustino <marifaustino@gmail.com>
2023-03-13 16:13:16 -0300

    configurações de CSS para Navbar e Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interesse!</h3>
         <p>MiniBlog &copy; 2023</p>
     </footer>
